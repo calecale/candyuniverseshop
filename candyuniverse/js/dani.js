112c1550daa2bcfe1648f337f605e54e025e9d91
Aleksandra Mitic <144780764+calecale@users.noreply.github.com>
2024-06-04 22:38:54 +0200

    Delete candyuniverse directory

--- candyuniverse/js/dani.js
+++
@@ -1,8 +0,0 @@
-function danasnjiDan(){
-    var danas= new Date();
-    var dan=danas.getDate();
-    var mesec=danas.getMonth()+1;
-    var godina=danas.getFullYear();
-  
-    document.write(dan+"."+mesec+"."+godina+".");
-  }
